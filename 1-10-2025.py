f25ef5856de20c7614f7c4cfe44ee7d6a021da27
Sindhukeshaboina <keshaboinasindhu@gmail.com>
2025-12-05 19:26:04 +0530

    Add files via upload

--- 1-10-2025.py
+++ 1-10-2025.py
@@ -1,105 +1,105 @@
-# def addition(num1=0,num2=0,num3=0):
-#     c = num1+num2+num3
-#     if c==0:
-#         return "no values passed"
-#     else:
-#         return c
+def addition(num1=0,num2=0,num3=0):
+    c = num1+num2+num3
+    if c==0:
+        return "no values passed"
+    else:
+        return c
     
-# print(addition(10,20))
-# print(addition(20))
-# print(addition(1,2,3))
-# print(addition())
+print(addition(10,20))
+print(addition(20))
+print(addition(1,2,3))
+print(addition())
 
 
 
 
-# def details(name,age,salary):
-#     text = f"Name: {name}, Age:{age}, Salary:{salary}"
-#     return text
+def details(name,age,salary):
+    text = f"Name: {name}, Age:{age}, Salary:{salary}"
+    return text
     
-# print(details(12,2500,"Arun"))
+print(details(12,2500,"Arun"))
 
 
-# def addition(*args):
-#     arguments = len(args)
-#     if arguments == 0:
-#         return "No values passed"
+def addition(*args):
+    arguments = len(args)
+    if arguments == 0:
+        return "No values passed"
     
-#     return f"sum of {arguments} values is {sum(args)}"
+    return f"sum of {arguments} values is {sum(args)}"
 
-# print(addition(1,10,3,5,6))
-# print(addition(1,10))
-# print(addition())
-
-
-
-
-# def fibonacci(num):
-#     a,b=0,1
-#     i=1
-#     while i<=num:
-#         print(a,end=" ")
-#         # c=a+b
-#         # a=b
-#         # b=c
-#         a,b=b,a+b
-#         i+=1
-# n = int(input("Enter series: "))
-# fibonacci(n)
+print(addition(1,10,3,5,6))
+print(addition(1,10))
+print(addition())
 
 
 
 
-
-# def fibonacci(num):
-#     res=""
-#     a,b=0,1
-#     while a<=num:
-#         res=res+str(a)+" "
-#         # c=a+b
-#         # a=b
-#         # b=c
-#         a,b=b,a+b
-#     return res
-# n = int(input("Enter series: "))
-# print(fibonacci(n))
-
-#swapping of 2 nos
-# num1 = int(input("Enter a number: "))
-# num2 = int(input("Enter a number: "))
-# print (f"num1: {num1} and num2:{num2}")
-# # c = num1
-# # num1 = num2
-# # num2 = c
-# num1,num2 =num2,num1 # another logic 
-# print (f"num1: {num1} and num2: {num2}")
+def fibonacci(num):
+    a,b=0,1
+    i=1
+    while i<=num:
+        print(a,end=" ")
+        # c=a+b
+        # a=b
+        # b=c
+        a,b=b,a+b
+        i+=1
+n = int(input("Enter series: "))
+fibonacci(n)
 
 
-# letter = input ("enter a letter :")
-# print ((letter >= 'A'and letter <='Z') or (letter >= 'a' and letter <='z'))
 
 
-# num1 = int (input ("enter a number : "))
-# if num1 | 1==0:
-#     print(f"{num1}is an odd")
-# else:
-#     print(f"{num1}is even")
 
-# num = int (input ("enter a number :"))
-# if num%2:
-#     print("odd number")
-#     print(f"{num}'s last digit is: {num%10}")
-#     if num>=1000:
-#         print ("is more than 3 digits number")
-#     elif num>=100:
-#         print("is 3 digit number")
-#     elif num>=10:
-#         print("is 2 digit number")
-#     else:
-#         print("is a 1 digit number")
-# else:
-#     print("even number")
-#     print(f"{num}'s last digit is:{num%10}")
+def fibonacci(num):
+    res=""
+    a,b=0,1
+    while a<=num:
+        res=res+str(a)+" "
+        # c=a+b
+        # a=b
+        # b=c
+        a,b=b,a+b
+    return res
+n = int(input("Enter series: "))
+print(fibonacci(n))
+
+# swapping of 2 nos
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+print (f"num1: {num1} and num2:{num2}")
+# c = num1
+# num1 = num2
+# num2 = c
+num1,num2 =num2,num1 # another logic 
+print (f"num1: {num1} and num2: {num2}")
+
+
+letter = input ("enter a letter :")
+print ((letter >= 'A'and letter <='Z') or (letter >= 'a' and letter <='z'))
+
+
+num1 = int (input ("enter a number : "))
+if num1 | 1==0:
+    print(f"{num1}is an odd")
+else:
+    print(f"{num1}is even")
+
+num = int (input ("enter a number :"))
+if num%2:
+    print("odd number")
+    print(f"{num}'s last digit is: {num%10}")
+    if num>=1000:
+        print ("is more than 3 digits number")
+    elif num>=100:
+        print("is 3 digit number")
+    elif num>=10:
+        print("is 2 digit number")
+    else:
+        print("is a 1 digit number")
+else:
+    print("even number")
+    print(f"{num}'s last digit is:{num%10}")
 
 
 # num =[2,4,5,6,2,7,1,-9]
@@ -137,4 +137,4 @@
 # while m<=-1:
 #     print(m,end=" ")
 #     m+=1
-# print()
+print()
